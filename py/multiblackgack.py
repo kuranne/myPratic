838590c64a5cf959842643232de51642cf44f5f9
kuranne <kurannemaisa@outlook.com>
2025-09-11 02:38:45 +0700

    Fix: bug in multibj

--- py/multiblackgack.py
+++ py/multiblackgack.py
@@ -1,12 +1,12 @@
 import random as rd
 import time
 import os
-import sys
 
 winner, turn, cheats, allbot = None, 1, input("Want to cheat? Y/N: "), False
 
 usedcard = []
 prewin = []
+alchk = []
 
 life = True
 alllose = 0
@@ -29,6 +29,17 @@ class Hand:
         self.cadidate = True
         self.stillplay = True
     
+    def chkifexceed(self):
+
+        if sum(self.number) > 21 and 11 in self.number:
+            for i in range(len(self.number)):
+                if self.number[i] == 11:
+                    self.number[i] = 1
+                    break
+
+        self.cadidate = False if sum(self.number) > 21 else True
+        return False if sum(self.number) > 21 else True
+
     def draw(self):
         global usedcard
         while True:
@@ -40,27 +51,17 @@ class Hand:
             pksymbl = int(rd.randrange(1,5,1))
             pkten = int(rd.randrange(1,5,1))
             theten = {1:"K",2:"Q",3:"J",4:pick}
-            while_ten =  theten[pkten]
+            while_ten = theten[pkten]
 
-            toOut = f"{"A" if pick == 1 or pick == 11 else while_ten if pick == 10 else pick }{symbl[pksymbl]}"
+            toOut = f"{'A' if pick == 1 or pick == 11 else while_ten if pick == 10 else pick }{symbl[pksymbl]}"
             if self.symbol in usedcard:
                 continue
-
+            
             usedcard.append(toOut)
             self.number.append(pick)
             self.symbol.append(toOut)
             break
-
-    def chkifexceed(self):
-
-        if sum(self.number) > 21 and 11 in self.number:
-            for i in range(len(self.number)):
-                if self.number[i] == 11:
-                    self.number[i] = 1
-                    break
-
-        self.cadidate = False if sum(self.number) > 21 else True
-        return False if sum(self.number) > 21 else True
+            
     
     def inTurn(self, n):
         global revals
@@ -98,11 +99,14 @@ while True:
 
 
 if allbot:
-    player = list({"name":"Bot", "hand":Hand(), "isBot":True}for i in range(szplayer))
+    player = list({"name":f"Bot{i+1}", "hand":Hand(), "isBot":True, "canplay":True}for i in range(szplayer))
 else:
-    player = list({"name":input(f"player {i+1}'s name(type bot for Bot): "), "hand":Hand(), "isBot":False}for i in range(szplayer))
+    player = list({"name":input(f"player {i+1}'s name(type bot for Bot): "), "hand":Hand(), "isBot":False, "canplay":True}for i in range(szplayer))
+    w = 0
     for i in player:
         if i["name"] == "bot" or i["name"] == "Bot":
+            i["name"] = f"{i['name']}{w}"
+            w+=1
             i["isBot"] = True
 
 while life:
@@ -111,26 +115,28 @@ while life:
     if alllose == szplayer:
         break
     for p in player:
-        if p["hand"].stillplay and not p["isBot"]:
-            os.system("cls" if os.name == "nt" else "clear")
-            if cheats or allbot:
-                for d in player:
-                    print(f"{d["name"]}: {", ".join(d["hand"].symbol)} >> {sum(d['hand'].number)}")
-            p["hand"].inTurn(p["name"])
-        elif p["hand"].stillplay and p["isBot"]:
-            if sum(p["hand"].number) >= 17:
-                p["hand"].stillplay = False
-                revals += 1
-            else:
-                p["hand"].draw()
-        if not p["hand"].cadidate and not p["hand"].chkifexceed():
-                alllose += 1
-        elif p["hand"].cadidate:
-            p["hand"].chkifexceed()
+        if p["name"] not in alchk:    
+            if p["hand"].stillplay and not p["isBot"]:
+                os.system("cls" if os.name == "nt" else "clear")
+                if cheats or allbot:
+                    for d in player:
+                        print(f"{d["name"]}: {", ".join(d["hand"].symbol)} >> {sum(d['hand'].number)}")
+                p["hand"].inTurn(p["name"])
+            elif p["hand"].stillplay and p["isBot"]:
+                p["hand"].chkifexceed()
+                if sum(p["hand"].number) >= 17:
+                    p["hand"].stillplay = False
+                    revals += 1
+                else:
+                    p["hand"].draw()
+            elif p["canplay"]:
+                    p["canplay"] = False
+                    alchk.append(p["name"])
+                    alllose += 1
     turn += 1 
 
 for p in player:
-    if p["hand"].cadidate:
+    if p["hand"].cadidate and sum(p["hand"].number) <= 21:
         prewin.append({"name":p["name"], "score":sum(p["hand"].number)})
 
 if len(prewin) > 1:
@@ -138,6 +144,14 @@ if len(prewin) > 1:
 winner = prewin[0]["name"]
 
 os.system("cls" if os.name == "nt" else "clear")
-print(f"The Winner is {winner} with {prewin[0]['score']} point.")
+try:
+    if prewin[0]["score"] == prewin[1]["score"]:
+        print("There is no winner")
+    else:
+        print(f"The Winner is {winner} with {prewin[0]['score']} point.") if szplayer > 2 else print(f"The Winner is {winner} with {prewin[0]['score']} point.")
+except:
+    IndexError
+    print("There is no winner")
 for p in player:
     print(f"{p["name"]}: {', '.join(p["hand"].symbol)} >> {sum(p["hand"].number)}")
+time.sleep(3)
