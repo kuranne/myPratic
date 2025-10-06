9a98f2c638b9893b790d8d87ebe7d6e781466f25
kuranne <kurannemaisa@outlook.com>
2025-10-06 18:07:34 +0700

    basic srectnumber from examination

---
+++ c/cs102/basicsrcretnumber.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    //initial variable
+    int i = 0, temp, ans = 0;
+    long long input; // ใช้ long long int เพราะโจทย์ input เลขเกิน int
+    scanf("%lld", &input); // %lld is long long int >> ~64bit
+
+    while (input > 0) { // divide until equal 0 แบบว่าดึงตัวหลังสุดออกมาเรื่อยๆจนกว่าจะหมด
+        
+        if (i%2==0){ 
+            temp = input % 10;
+        } else {
+            ans += (temp - (input % 10))*(temp - (input % 10))*(temp - (input % 10))*(temp - (input % 10)); //(a-b)**4
+        }
+
+        input /= 10;
+        i++; // for check the length of input
+    }
+
+    if (i%2==0){ // if length is not even (not couple) print error
+        printf("%d\n", ans);
+    } else {
+        printf("INVALID TEXT\n");
+    }
+
+    return 0;
+}
