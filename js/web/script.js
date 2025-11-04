ea42d648e82bf3e3821f2e2bb758f7a8893ca99e
kuranne <kurannemaisa@outlook.com>
2025-11-04 20:34:52 +0700

    Programming in th: plate in rust

--- js/web/script.js
+++
@@ -1,9 +0,0 @@
-document.writeln('<h4>Script Loaded!</h4>')
-
-function changeText() {
-    document.getElementById("to-change-text").innerText = "Yay, You Change THISS!";
-}
-
-function darkMode() {
-    document.body.classList.toggle("dark")
-}
